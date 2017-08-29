88312d81a6008c8315f485f454b71db05ffc8b72
Tier5 LLC <jon@thinkvaughn.com>
2017-08-29 16:08:24 +0530

    basic sass example

---
+++ readme.md
@@ -0,0 +1,8 @@
+## Basic Demo of ass and laravel-mix in Laravel 5.4
+ 	npm install
+
+ 	npm run watch
+
+## License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+	   <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link type="text/css" rel="stylesheet" href="{{asset('css/app.css')}}"/>
+	 <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+ {{-- <link href="css/app.css" rel="stylesheet" type="text/css"> --}}
+</head>
+<body>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<ul>
+	 
+	<li class="box"></li>
+	<li class="box"></li>
+	<li class="box"></li>
+</ul>
+ 
+</body>
+</html>
